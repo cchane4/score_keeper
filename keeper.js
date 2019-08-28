77f1832f04965f4072ffa557659cb53550f643dc
Chad Chaney <cchane4@gmail.com>
2019-08-28 15:40:38 -0500

    getting the player 1 score feature to work

--- keeper.js
+++ keeper.js
@@ -1,11 +1,12 @@
+
 let player_1 = document.getElementById("p1"); 
 let p1_score = 0; 
-let h1 = document.querySelector("h1"); 
+let p1_score_display = document.querySelector("#p1_score_display"); 
+
 
 
 player_1.addEventListener("click", function(){ 
     p1_score++; 
-console.log("p1_score"); 
-h1.textContent = p1_score; 
+ p1_score_display.textContent = p1_score; 
 }); 
 
