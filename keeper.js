391f523a49ec824e40f00e7371b8017da6463e23
Chad Chaney <cchane4@gmail.com>
2019-08-28 16:15:44 -0500

    adding game over logic

--- keeper.js
+++ keeper.js
@@ -5,16 +5,29 @@ let p1_score = 0;
 let p2_score = 0; 
 let p1_score_display = document.querySelector("#p1_score_display"); 
 let p2_score_display = document.querySelector("#p2_score_display"); 
+let game_over = false; 
+let winning_score = 5; 
 
 
 
 player_1.addEventListener("click", function(){ 
-    p1_score++; 
+if (!game_over){ 
+  p1_score++; 
+if (p1_score === winning_score){ 
+    game_over = true; 
+}
  p1_score_display.textContent = p1_score; 
+} 
 }); 
+
 
 player_2.addEventListener("click", function(){ 
-    p2_score++; 
- p2_score_display.textContent = p2_score; 
-}); 
-
+    if (!game_over){ 
+        p2_score++; 
+      if (p2_score === winning_score){ 
+          game_over = true; 
+      }
+       p2_score_display.textContent = p2_score; 
+      } 
+      }); 
+      
