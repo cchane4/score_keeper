3a46ad4ba013f06e96372df99a8faaa92ffc6cfc
Chad Chaney <cchane4@gmail.com>
2019-08-28 16:04:50 -0500

    adding player 2 functionality

--- keeper.js
+++ keeper.js
@@ -1,12 +1,20 @@
 
 let player_1 = document.getElementById("p1"); 
+let player_2 = document.getElementById("p2"); 
 let p1_score = 0; 
+let p2_score = 0; 
 let p1_score_display = document.querySelector("#p1_score_display"); 
+let p2_score_display = document.querySelector("#p2_score_display"); 
 
 
 
 player_1.addEventListener("click", function(){ 
     p1_score++; 
  p1_score_display.textContent = p1_score; 
+}); 
+
+player_2.addEventListener("click", function(){ 
+    p2_score++; 
+ p2_score_display.textContent = p2_score; 
 }); 
 
